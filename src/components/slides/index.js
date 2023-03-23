0751b3b8d3cd250e2a06125fc7e11f471f463925
B S M <thanhbilly.94@gmail.com>
2023-03-23 16:51:45 +0700

    add code to app

---
+++ src/components/slides/index.js
@@ -0,0 +1,131 @@
+import React, { useState, useEffect, useReducer } from "react";
+
+import {
+  SliderItem,
+  SliderContainer,
+  SliderWrapper,
+  Navigation,
+  NavigationItem,
+  ControlLeft,
+  ControlRight,
+} from "./style.js";
+
+const HorizontalSlideShow = () => {
+  const width = useWindowWidth();
+  const [state, dispatch] = useReducer(reducer, {
+    currentIndex: 2,
+    items: [
+      { id: 1, name: "Information 1" },
+      { id: 2, name: "Information 2" },
+      { id: 3, name: "Information 3" },
+      { id: 4, name: "Information 4" },
+      { id: 5, name: "Information 5" },
+    ],
+  });
+
+  return (
+    <div>
+      <SliderContainer className={"slider-instance"} height={"500px"}>
+        <SliderWrapper
+          width={width * state.items.length}
+          style={{
+            transform: `translateX(${-(state.currentIndex * width)}px)`,
+            transition: "transform ease-out 0.30s",
+            width: width * state.items.length + "px",
+          }}
+        >
+          {state.items.map((i, index) => {
+            return (
+              <Slide
+                key={i.id}
+                last={index === state.items.length - 1}
+                index={index}
+                item={i}
+                dispatch={dispatch}
+                snap={state.snap}
+                width={width}
+              />
+            );
+          })}
+        </SliderWrapper>
+
+        <Navigation>
+          {state.items.map((i, index) => {
+            return (
+              <NavigationItem
+                active={index === state.currentIndex}
+                onClick={() => dispatch({ type: "GOTO", index })}
+                key={"nav" + i.id}
+              >
+                &nbsp;
+              </NavigationItem>
+            );
+          })}
+        </Navigation>
+        <div>
+          {state.currentIndex > 0 && (
+            <ControlLeft onClick={() => dispatch({ type: "PREV" })}>
+              prev
+            </ControlLeft>
+          )}
+
+          {state.currentIndex < state.items.length - 1 && (
+            <ControlRight onClick={() => dispatch({ type: "NEXT" })}>
+              next
+            </ControlRight>
+          )}
+        </div>
+      </SliderContainer>
+    </div>
+  );
+};
+
+function useWindowWidth() {
+  const [width, setWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => setWidth(window.innerWidth);
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  });
+  return width;
+}
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "NEXT":
+      return {
+        ...state,
+        currentIndex: state.currentIndex + (1 % state.items.length),
+      };
+    case "PREV":
+      return {
+        ...state,
+        currentIndex: state.currentIndex - (1 % state.items.length),
+      };
+    case "GOTO":
+      return {
+        ...state,
+        currentIndex: action.index,
+      };
+    case "RESET":
+      return { currentIndex: 0, currentPosition: 0 };
+
+    default:
+      return state;
+  }
+}
+
+const Slide = ({ item, width }) => {
+  return (
+    <SliderItem width={width}>
+      <div>{item.name}</div>
+    </SliderItem>
+  );
+};
+
+export default HorizontalSlideShow;
---
+++ src/components/slides/style.js
@@ -0,0 +1,80 @@
+import styled, { css } from "styled-components";
+
+export const SliderContainer = styled.div`
+  position: relative;
+  background: #fafafa;
+  overflow: hidden;
+  width: 100%;
+
+  height: ${(props) => props.height || "500px"};
+`;
+
+export const SliderWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+export const SliderItem = styled.div`
+  position: relative;
+  height: 500px;
+  width: ${(props) => props.width + "px" || "100%"};
+  border: 1px solid #000;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 40px;
+`;
+
+export const Navigation = styled.ul`
+  position: relative;
+  text-align: center;
+  top: 12px;
+  margin: 0;
+  padding: 0;
+  justify-content: center;
+  display: flex;
+`;
+
+export const NavigationItem = styled.li`
+  list-style: none;
+  width: 10px;
+  height: 10px;
+  margin: 0 3px;
+  background: rgba(195, 216, 250);
+  border-radius: 100%;
+  cursor: pointer;
+
+  ${(props) =>
+    props.active &&
+    css`
+      background: rgba(68, 103, 193);
+      width: 12px;
+      height: 12px;
+    `};
+`;
+
+export const Control = styled.div`
+  position: absolute;
+  top: 0;
+  width: 40px;
+  height: 40px;
+  margin: 10px;
+  cursor: pointer;
+`;
+
+export const ControlLeft = styled(Control)`
+  left: 0;
+`;
+export const ControlRight = styled(Control)`
+  right: 0;
+`;
+
+export const BigElement = styled.div`
+  min-height: 1000px;
+  background: #61dafb;
+  width: 30px;
+`;
